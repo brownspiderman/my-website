6d45f314899b9c460683da9824f40282a92f74b0
himanyabehl@gmail.com <swordfish@192.168.1.31>
2026-07-24 13:42:42 +0530

    vite, deploy.yml update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/my-website/', // Replace 'my-website' with your exact GitHub repository name
+  base: 'https://github.com/brownspiderman/my-website', // Replace 'my-website' with your exact GitHub repository name
 })
